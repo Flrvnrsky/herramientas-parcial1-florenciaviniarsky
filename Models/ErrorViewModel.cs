b0d4af1d56c8fabf5498c6ad2cfffdfafc51ed26
Flrvnrsky <florlkv@hotmail.com>
2023-10-09 22:35:11 -0300

    Cambios 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace parcialherramientas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
